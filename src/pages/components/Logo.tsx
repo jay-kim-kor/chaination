78646e1cb85b3b50fdd7c7f5b234d1bb538e69fc
TaeckH <tjsk2222@gmail.com>
2023-04-09 03:42:22 +0900

    Revert "[Update] 사이트 로고 위치 임시 설정"
    This reverts commit 2bdbb874dc37a7cced5a459ae95dda6cfb30512b.

--- src/pages/components/Logo.tsx
+++ src/pages/components/Logo.tsx
@@ -1,25 +1,9 @@
 const Logo = () => {
-  return (
-    <div
-      style={{
-        background: "#FFCCFF",
-        textAlign: "center",
-        marginTop: "20px",
-      }}
-    >
-      <h2 style={{ margin: "0 auto", color: "white"}}>Logo</h2>
-      <hr
-        style={{
-          width: "90%",
-          color: "white",
-          margin: "auto",
-          marginTop: "20px",
-          marginBottom: "20px",
-          border: "1px solid black",
-        }}
-      />
-    </div>
-  );
-};
+    return (
+        <div>
+            <h2>사이트 얼릉 만들어</h2>
+        </div>
+    )
+}
 
-export default Logo;
+export default Logo
