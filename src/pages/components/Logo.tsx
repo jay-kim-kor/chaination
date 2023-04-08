6ddcfe299e62b130ebbd4f65b38f6cb0c490aca4
TaeckH <tjsk2222@gmail.com>
2023-04-08 17:52:32 +0900

    [Update] 사이트 로고 위치 임시 설정

--- src/pages/components/Logo.tsx
+++ src/pages/components/Logo.tsx
@@ -1,9 +1,25 @@
 const Logo = () => {
-    return (
-        <div>
-            <h2>사이트 얼릉 만들어</h2>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        background: "#FFCCFF",
+        textAlign: "center",
+        marginTop: "20px",
+      }}
+    >
+      <h2 style={{ margin: "0 auto", color: "white"}}>Logo</h2>
+      <hr
+        style={{
+          width: "90%",
+          color: "white",
+          margin: "auto",
+          marginTop: "20px",
+          marginBottom: "20px",
+          border: "1px solid black",
+        }}
+      />
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
