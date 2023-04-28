9e07b1aa8d3619d456a340a9a63093dac25496a8
Saelma <rlarlgus300@gmail.com>
2023-04-29 02:11:50 +0900

    useEffect의 과도한 추적으로 인해 검색어가 변영될 때마다 렌더링하게 구현

--- src/app/(AppComponents)/CampaignList.tsx
+++ src/app/(AppComponents)/CampaignList.tsx
@@ -1,9 +1,9 @@
-"use client";
-import { useState } from "react";
+"use client"
+
+import { useState, useEffect } from "react";
 import { ICampaignCardProps } from "./CampaignCard";
 import CampaignCard from "./CampaignCard";
-import SearchBar from "./SearchBar"
-
+import SearchBar from "./SearchBar";
 
 interface CampaignListProps {
   campaigns: ICampaignCardProps[];
@@ -11,30 +11,40 @@ interface CampaignListProps {
 
 const CampaignList: React.FC<CampaignListProps> = ({ campaigns }) => {
   const [searchValue, setSearchValue] = useState("");
-
-  const beneficiaries: string[] = campaigns.map((campaign) => campaign.beneficiary);
-
-  const filteredCampaigns = campaigns.filter((campaign) =>
-    campaign.title.toLowerCase().includes(searchValue.toLowerCase())
-  );
+  const [beneficiaries, setBeneficiaries] = useState<string[]>([]);
+  const [filteredCampaigns, setFilteredCampaigns] = useState<ICampaignCardProps[]>(campaigns);
 
   const onSearch = (value: string) => {
     setSearchValue(value);
   };
 
+  useEffect(() => {
+    const newCampaigns = campaigns.filter((campaign) =>
+      campaign.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredCampaigns(newCampaigns);
+  }, [campaigns, searchValue]);
+
+  useEffect(() => {
+    const updatedBeneficiaries = filteredCampaigns.map(
+      (campaign) => campaign.beneficiary
+    );
+    setBeneficiaries(updatedBeneficiaries);
+  }, [filteredCampaigns]);
+
   return (
     <>
-    <SearchBar onSearch={onSearch} />
-    <div className="flex-col justify-center items-center space-y-4">
-      {filteredCampaigns.map((campaign: ICampaignCardProps, index: number) => (
-        <CampaignCard
-          {...campaign}
-          key={campaign.id}
-          beneficiary={beneficiaries}
-          index={index}
-        />
-      ))}
-    </div>
+      <SearchBar onSearch={onSearch} />
+      <div className="flex-col justify-center items-center space-y-4">
+        {filteredCampaigns.map((campaign: ICampaignCardProps, index: number) => (
+          <CampaignCard
+            {...campaign}
+            key={campaign.id}
+            beneficiary={beneficiaries}
+            index={index}
+          />
+        ))}
+      </div>
     </>
   );
 };
