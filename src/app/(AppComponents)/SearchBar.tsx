91600a1ef8f26034548c4a711d8853b26e0a48cd
Saelma <rlarlgus300@gmail.com>
2023-04-28 23:18:34 +0900

    검색 입력시 Tilte을 기준으로 검색에 해당하는 title 빨갛게 표시

---
+++ src/app/(AppComponents)/SearchBar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useState } from "react";
+
+const SearchBar: React.FC = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+    // Emit a custom event with the search value.
+    const searchEvent = new CustomEvent("searchValueChange", { detail: event.target.value });
+    window.dispatchEvent(searchEvent);
+  };
+
+  return (
+    <form className="mb-4">
+      <input
+        type="text"
+        value={searchValue}
+        onChange={handleSearchValueChange}
+        className="border border-gray-300 p-2 rounded"
+        placeholder="캠페인 검색"
+      />
+    </form>
+  );
+};
+
+export default SearchBar;
+
