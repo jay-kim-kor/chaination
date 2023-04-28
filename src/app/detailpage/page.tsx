60429769daff8030e6a93e7e95fbf77591f73526
TaeckH <tjsk2222@gmail.com>
2023-04-28 16:16:32 +0900

    상세 페이지 좌우 요소 간 크기 조정

--- src/app/detailpage/page.tsx
+++ src/app/detailpage/page.tsx
@@ -11,20 +11,26 @@ export default function Page() {
             <button className="bg-white hover:bg-red-300 hover:text-white transition duration-500 ease-in-out transform hover:scale-110 rounded-full px-4 py-2">캠페인 내용</button>
             <button className="bg-white hover:bg-red-300 hover:text-white transition duration-500 ease-in-out transform hover:scale-110 rounded-full px-4 py-2">기부 내역</button>
         </div>
-        <div className="flex items-center justify-evenly w-full h-full pt-8">
-          <div className="pl-80 w-1/2">
-            <div className="flex items-center justify-center w-full h-[64rem] rounded-b-lg shadow-md p-4 relative">
+        <div className="flex w-full h-full justify-center">
+        <div className="flex items-center justify-evenly w-5/6 h-full pt-8">
+
+          {/* 왼쪽 영역 */}
+          <div className="w-3/5">
+            <div className="flex items-center justify-center w-full h-[64rem] rounded-b-lg shadow-md p-4 bg-red-300 relative">
               <img src="https://www.ghibli.jp/gallery/umi005.jpg" className="w-full h-64 object-cover object-center absolute top-0 left-0"/>
               <p>캠페인 내용을 적을 수 있는 구역</p>
             </div>
           </div>
-          <div className="pl-12 w-1/3 border-l">
+
+          {/* 오른쪽 영역 */}
+          <div className="w-1/4 border-l flex justify-center">
           <div className="w-72 h-[28rem] rounded-lg shadow-md p-4 border border-red-300 relative">
             <div className="relative inline-block w-12 h-12">
               <div className="absolute top-0 left-0 bg-red-300 rounded-md w-full h-full flex justify-center items-center">
                 <p className="text-white text-center text-sm">D-14</p>
               </div>
             </div>
+
               <p>기부하기 기능이 들어간 카드에 담겨지는 내용 작성란</p>
               <p className="my-4">캠페인에 후원되어 있는 현재 금액: 1000원</p>
             <div className="w-full bg-gray-200 h-2 rounded-lg relative">
@@ -35,6 +41,8 @@ export default function Page() {
             </div>
             </div>
         </div>
+        </div>
+        
         
         <div className="flex items-center justify-center h-screen">
         <div className="mx-auto border-t pt-8">
