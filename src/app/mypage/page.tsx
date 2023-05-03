a8667bf9acf249ceb674d3280ba9422d9a1f8077
MoYeaHo <cmg7374@gmail.com>
2023-05-03 23:04:59 +0900

    UI기본 틀 지정 및 디자인 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -10,11 +10,12 @@ import { campaigns } from '../page'
 
 type Props = {};
 
+//캠페인카드를 불러오는 함수
 export default function MyInfo({ }: Props) {
   const [nowDonatingsState, setNowDonatingsState] = useState<boolean[]>([]);
   const beneficiaries: string[] = campaigns.map((campaign, index) => campaign.beneficiary);
   const nowDonatings: boolean[] = campaigns.map((campaign, index) => campaign.nowDonating);
-
+  const [googleUser, setGoogleUser] = useState(null);
   const [accountId, setAccountId] = useState<string>("");
 
   useEffect(() => {
@@ -45,58 +46,92 @@ export default function MyInfo({ }: Props) {
 
   }, [accountId]);
 
-
   return (
-    <div className="container mx-auto px-4 pt-24">
-      {/*메타마스크를 표현하는 그림 */}
-      <img src="/02.png" alt="METAMASK" className="object-cover" />
+    <>
 
-
-      {/*메타마스크와 연동시켜 내 계정 가져오기*/}
-      <div className="flex items-center justify-center">
-        <input
-          type="text"
-          className="flex-grow bg-white rounded-lg shadow-md px-10 py-3 text-lg font-bold text-black placeholder-gray-400 focus:outline-none focus:ring-4 focus:ring-red-500 focus:border-transparent"
-          value={accountId}
-          readOnly // 수정이 불가하게 변경
-          onChange={(e) => setAccountId(e.target.value)}
-          placeholder="메타마스크에 로그인하세요."
-        />
-      </div>
-
-      {/* 내 기록을 알려주는 글*/}
-      <div className="flex items-center justify-center">
-        <h1 className="text-2xl font-bold">
+      <div className="flex items-center justify-center w-full h-25 border-b px-8 py-8">
+        <div className="bg-red-300 rounded-full text-2xl font-bold mx-auto px-4 py-2">
+          내정보
+        </div>
+        <div className="bg-red-300 rounded-full text-2xl font-bold mx-auto px-4 py-2">
           참여 기록
-        </h1>
-      </div>
-
-      {/* 캠페인카드 불러오기 */}
-      <div>         
-        <div className="flex-col justify-center items-center space-y-4">
-        {/* 미리 작성된 campaings 배열을 기반으로 렌더링 */}
-        {campaigns.filter((campaign, index) => nowDonatingsState[index]).map((campaign: ICampaignCardProps, index: number) => (
-          <CampaignCard
-            {...campaign}
-            key={campaign.id}
-            beneficiary={beneficiaries}
-            index={index}
-          />
-        ))}
         </div>
-      
-      {/* 캠페인카드 옆에 캠페인에 내가 기부한 금액 */}
-        <div >
+        <div className="mx-auto">
 
         </div>
       </div>
-      
-      {/* 내 전체 기록 */}
-      <div>
+      <div className="flex w-full justify-center ">
+
+        {/*기본적인 내 정보*/}
+        <div className="flex w-1/5 h-[1200px] mx-8">
+
+          <div className="pt-6">
+            {/* 연결된 계정에서 이름 가져오기*/}
+            <div>
+              이름
+            </div>
+            {/* 현재 로그인 되어있는 ID*/}
+            <div>
+              ID
+            </div>
+
+            <div className="border-4 w-full h-auto">
+              {/*메타마스크 로고 */}
+              <img src="/02.png" alt="METAMASK" className="object-cover w-auto" />
+            </div>
+
+            <div className="flex flex items-center justify-center">
+              {/*메타마스크와 연동시켜 내 계정 가져오기*/}
+              <div className=" ">
+                <input
+                  type="text"
+                  className="flex-grow bg-white rounded-lg shadow-md px-10 py-3 text-base font-bold text-black placeholder-gray-400 focus:outline-none focus:ring-4 focus:ring-red-500 focus:border-transparent"
+                  value={accountId}
+                  readOnly // 수정이 불가하게 변경
+                  onChange={(e) => setAccountId(e.target.value)}
+                  placeholder="메타마스크에 로그인하세요."
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 캠페인카드 불러오기 , 참여기록이 없을때 표시창 필요함*/}
+        <div className="flex w-4/5 h-[1200px] border-r bg-red-300">
+          {campaigns.filter((campaign, index) => nowDonatingsState[index]).length === 0 ? (
+            <div className="flex flex-col justify-center items-center w-full h-full">
+              <h2 className="text-2xl  font-bold">
+                조건에 맞는 캠페인 카드가 없습니다.</h2>
+            </div>
+          ) : (
+            <>
+              <div className="flex-col items-center  p-4 pt-24 space-y-4">
+                {/* 미리 작성된 campaings 배열을 기반으로 렌더링 */}
+                {campaigns.filter((campaign, index) => nowDonatingsState[index]).map((campaign: ICampaignCardProps, index: number) => (
+                  <CampaignCard
+                    {...campaign}
+                    key={campaign.id}
+                    beneficiary={beneficiaries}
+                    index={index}
+                  />
+                ))}
+              </div>
+              <div className="mx-auto px-4">
+                <div className="flex items-center justify-center">
+                  {/* 캠페인카드 옆에 캠페인에 내가 기부한 금액 */}
+                  <div>캠페인카드 공간</div>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+
+        {/* 빈공간 */}
+        <div className="flex w-1/3 ">
+
+        </div>
 
       </div>
-    </div>
-
-
+    </>
   );
 }
